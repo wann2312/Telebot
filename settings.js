5ccad7c86fdac69945dc547a0f5ac4898308858b
wann2312 <154908556+wann2312@users.noreply.github.com>
2024-02-19 01:12:36 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
 global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_NAME = "WanBOT" //your bot name
+global.OWNER_NAME = "wanzz" //your name
+global.OWNER_NUMBER = "628984451463" //your telegram number
+global.OWNER = ["https://t.me/call_me_wann", "https://t.me/call_me_wann"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
