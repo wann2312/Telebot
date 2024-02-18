2a184a521a1eef6587c921d08718302241730fb2
wann2312 <154908556+wann2312@users.noreply.github.com>
2024-02-19 01:15:26 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_TOKEN = "6974189028:AAHApSnqYYul8-9LUmeVpWYxsOX_bJ53Xzw" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "WanBOT" //your bot name
 global.OWNER_NAME = "wanzz" //your name
 global.OWNER_NUMBER = "628984451463" //your telegram number
